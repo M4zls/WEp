ccfeec63be7d7cf426e42f41a0a428b76b27ae3b
M4zls <max.palma@duocuc.cl>
2026-04-25 23:06:30 -0400

    Convert line endings to LF

--- backend/App/src/index.ts
+++ backend/App/src/index.ts
@@ -1,15 +1,17 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { estudianteController } from './controllers/EstudiantesController.js';
+import { getDatabaseinstance } from './utils/data.js';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+getDatabaseinstance(); 
+
+app.route('/estudiantes/', estudianteController)
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3000,
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
