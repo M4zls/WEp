06528ccbddc1f2709e87a61d9f81cdb5a5d5a1ce
M4zls <max.palma@duocuc.cl>
2026-05-06 19:41:33 -0400

    cambios directorios

--- backend/App/dist/microservicios/notificaciones/src/models/data.d.ts
+++
@@ -1,5 +0,0 @@
-import Database from 'better-sqlite3';
-export declare const getDatabaseinstance: () => import("drizzle-orm/better-sqlite3").BetterSQLite3Database<Record<string, unknown>> & {
-    $client: Database.Database;
-};
-//# sourceMappingURL=data.d.ts.map
